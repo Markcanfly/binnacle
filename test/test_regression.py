f56ce84dd5cab8ed6fcc86200ae20239faa5963a
Mark Varga <varga.mrk@gmail.com>
2022-12-11 13:18:26 +0100

    Test added usage of value

--- test/test_regression.py
+++ test/test_regression.py
@@ -6,17 +6,30 @@ from deepdiff import Delta
 from helm.helmbin import Helm
 import helm.chart
 
-def regression(old: Path, new: Path, values: List[helm.chart.Values]) -> List[Delta]:
+def regression(old: str, new: str, values: List[str]) -> List[Delta]:
     path = Path('test/resources/charts')
     old_chart = helm.chart.HelmChart(path/old)
     new_chart = helm.chart.HelmChart(path/new)
-    helm_bin = Helm()
-    return command.regression.regression(helm_bin, old_chart, new_chart, values)
+    res_path = Path('test/resources/values')
+    values = [helm.chart.Values(res_path/p) for p in values]
+    return command.regression.regression(Helm(), old_chart, new_chart, values)
 
 class TestRegression(unittest.TestCase):
     
     def test_no_change(self):
         self.assertEqual([{}], regression('old', 'old', []))
+
+    def test_chart_change(self):
+        d = regression('old', 'new', [])
+        name_change = d[0]['values_changed']["root[0]['metadata']['name']"]
+        self.assertEqual('cat', name_change['old_value'])
+        self.assertEqual('dog', name_change['new_value'])
+
+    def test_value_change(self):
+        d = regression('old', 'new', [])
+        image_version = d[0]['values_changed']["root[0]['spec']['containers'][0]['image']"]
+        # New chart uses a value for the version instead of hardcoding it
+        self.assertEqual('nginx:1.14.2', image_version['new_value'])
 
 if __name__ == '__main__':
     unittest.main()
